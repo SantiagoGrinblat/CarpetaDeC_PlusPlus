7be52a5ba598c6ff8738800f3c8034707d7002f2
Santiago <santiago.dev21@gmail.com>
2026-04-16 10:31:55 -0300

    primer commit de c++

---
+++ AprendiendoC++.cpp
@@ -0,0 +1,3 @@
+//Cada vez que quiera mostrar la respuesta del codigo en la termina tengo que hacer.
+//todo: g++ [nombre del archivo].cpp -o main && ./main
+//cada vez que hago un cambio en el codigo necesito guardar el codigo, con ( command + s ) y luego volver a ejecutar el comando de arriba para ver los cambios reflejados en la terminal.
